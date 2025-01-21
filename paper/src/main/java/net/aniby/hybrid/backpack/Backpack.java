649210c8372f62e27dc16f85e30abb02c64ca8e6
An1by <anibycor@gmail.com>
2025-01-21 05:39:40 +0300

    Initial commit - hybrid mod

---
+++ paper/src/main/java/net/aniby/hybrid/backpack/Backpack.java
@@ -0,0 +1,17 @@
+package net.aniby.hybrid.backpack;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.UUID;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class Backpack {
+//    FileConfiguration configuration;
+
+    public Backpack(UUID uuid) {
+//        this.configuration = YamlConfiguration.loadConfiguration();
+    }
+}
---
+++ paper/src/main/java/net/aniby/hybrid/backpack/HybridBackpackPlugin.java
@@ -0,0 +1,15 @@
+package net.aniby.hybrid.backpack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HybridBackpackPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
